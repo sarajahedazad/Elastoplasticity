628e17c5cff09f95ae58ed57f5064666b058d90e
Sara Jahedazad <114187084+sarajahedazad@users.noreply.github.com>
2025-02-17 00:41:24 -0500

    Update test_elastoplasticity.py

--- tests/test_elastoplasticity.py
+++ tests/test_elastoplasticity.py
@@ -20,11 +20,20 @@ def test_episotropic():
   ep_iso.update_step( delta_epsilon, epsilon0, sigma0 )
   known = 18.991899189918996
   found = ep_iso.sigma
-  assert np.isclose( known , found ) 
+  assert np.isclose( known , found )
+
+  # E, H, Y0= 1000, 111, 10
+  # ep_iso = ep.ElastoPlasticIsoHard( E, H, Y0)
+  # delta_epsilon = 0.1
+  # epsilon0, sigma0 = 0, 0
+  # ep_iso.update_step( delta_epsilon, epsilon0, sigma0 )
+  # known = 18.991899189918996
+  # found = ep_iso.sigma
+  # assert np.isclose( known , found ) 
 
 def test_kinematic():
   E, H, Y= 1000, 111, 10
-  ep_k = ep.ElastoPlasticKinematicHard( E, H, Y0)
+  ep_k = ep.ElastoPlasticKinematicHard( E, H, Y)
   delta_epsilon = 0.001
   epsilon0, sigma0 = 0, 0
   ep_k.update_step( delta_epsilon, epsilon0, sigma0 )
